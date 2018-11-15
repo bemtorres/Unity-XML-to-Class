334bddb2a3ebf7d3c67047442716b34040d17ac1
Benjamin <benja.mora.torres@gmail.com>
2018-11-15 17:44:12 -0300

    1234

---
+++ Persona.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Persona : MonoBehaviour
+{
+    public int edad;
+    public int tiempo;
+    public int damage;
+    public int nivel;
+
+
+    public InputField edadE;
+    public InputField tiempoE;
+    public InputField damageE;
+    public InputField nivelE;
+    public Text mensaje;
+    public override string ToString()
+    {
+        string mensaje = String.Format("edad : {0} , tiempo : {1} , daño : {2} , nivel : {3} ", edad, tiempo, damage, nivel);
+        return mensaje;
+    }
+
+    public void Save()
+    {
+        this.edad = int.Parse(edadE.text);
+        this.tiempo = int.Parse(tiempoE.text);
+        this.damage = int.Parse(damageE.text);
+        this.nivel = int.Parse(nivelE.text);
+        XMLPersona.Guardar(this);
+        Load();
+    }
+    public void Load()
+    {
+        int[] load = XMLPersona.CargarPersona();
+
+        edad = load[0];
+        tiempo = load[1];
+        damage = load[2];
+        nivel = load[3];
+
+        mensaje.text = this.ToString();
+    }
+
+    void Start()
+    {
+        Debug.Log(Application.persistentDataPath); 
+    }
+
+}
---
+++ XMLPersona.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System;
+
+public static class XMLPersona { 
+    
+    public static void Guardar(Persona persona)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream stream = new FileStream(Application.persistentDataPath + "/persona.xml", FileMode.Create);
+
+        PersonaData data = new PersonaData(persona);
+        bf.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static int[] CargarPersona(){
+        if(File.Exists(Application.persistentDataPath + "/persona.xml"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream stream = new FileStream(Application.persistentDataPath + "/persona.xml", FileMode.Open);
+
+            PersonaData data = bf.Deserialize(stream) as PersonaData;
+            stream.Close();
+            return data.stats;
+
+        }
+        else
+        {
+            Debug.LogError("Archivo no existe");
+            return new int[4];
+        }
+    }
+ }
+
+[Serializable]
+public class PersonaData
+{
+    public int[] stats;
+
+    public PersonaData(Persona per)
+    {
+        stats = new int[4];
+        stats[0] = per.edad;
+        stats[1] = per.tiempo;
+        stats[2] = per.damage;
+        stats[3] = per.nivel;
+    }
+}
